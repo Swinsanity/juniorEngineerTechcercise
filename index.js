eaca77b72821cca162b2084e74ff52cd649113be
Jesse Swinson <jesseswinsanity@gmail.com>
2022-05-25 16:12:23 -0400

    Changed const to var for more adaptability

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-const res = require('express/lib/response');
+var res = require('express/lib/response');
 
-const express = require('express');
-const app = express();
-const PORT = 8080;
+
+var express = require('express');
+var app = express();
+var PORT = 8080;
 
 app.use( express.json() )
 
+// app.get('./transactions.json', (req, res) => {
+//     console.log(res)
 
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: '👕',
-        size: 'large'
-    })
-})
+//     res.sendFile(path.join(juniorEngineerTechcercise, './transactions.json'));
+// });
 
-app.post('/tshirt/:id', (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
 
-    if (!logo) {
-        res.status(418).send({ message: 'We need a logo!'})
+// app.get('/tshirt', (req, res) => {
+//     res.status(200).send({
+//         tshirt: '👕',
+//         size: 'large'
+//     })
+// })
 
-    }
+// app.post('/tshirt/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { logo } = req.body;
 
-    res.send({
-        tshirt: `👕 with your ${logo} and ID of ${id}`,
-    });
-});
+//     if (!logo) {
+//         res.status(418).send({ message: 'We need a logo!'})
+
+//     }
+
+//     res.send({
+//         tshirt: `👕 with your ${logo} and ID of ${id}`,
+//     });
+// });
 
 app.listen(
     PORT,

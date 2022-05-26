d86f2d8d4b5381878c3a7a68bba8bd71761e59a8
Jesse Swinson <jesseswinsanity@gmail.com>
2022-05-26 08:16:10 -0400

    Attempted to figure out why my JSON isn't filtering and am unsure. Started to add things to potentially Stream it, but I am unsure if that is actually the solution.

--- index.js
+++ index.js
@@ -3,78 +3,31 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const fs = require('fs');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+const jsonData1 = fs.readFileSync('transactions.json', 'utf-8')
+const parsedData1 = JSON.parse(jsonData1)
+const jsonData2 = fs.readFileSync('oneTransaction.json', 'utf-8')
+const parsedData2 = JSON.parse(jsonData2)
 
 app.use( express.json() )
 
-app.listen(
-    PORT,
-    () => console.log(`It's alive! on http://localhost:${PORT}`)
+app.listen(PORT, () => console.log(`It's alive! on http://localhost:${PORT}`)
 );
 
-app.get('/getTransactions', (req, res) => {
-    fs.readFile("transactions.json", 'utf-8', function(err, data){
-        console.log(data)
-        res.end(data);
-
-    })
-});
-
-app.get('/getFirstTransaction', (req, res) => {
+app.get('/transactions', (req, res) => {
     
-    fs.readFile("transactions.json", 'utf-8', (err, jsonString) => {
-        if (err) {
-            console.log(err);
-        } else {
-            try {
-                var data = JSON.parse(JSON.stringify(jsonString));
-                console.log(data.Id)
-                res.send(data.Id)
-            } catch (err) {
-                console.log('Error parsing JSON', err);
-            }
-            
-        }
-    })
-});
+    console.log(parsedData1);
+    res.send(parsedData1)
+    });
 
-// app.get('/getTransactions/:id', (req, res) => {
-//     var { id } = req.params
-    
-//     fs.readFile("transactions.json", 'utf-8', (err, jsonString) => {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             try {
-//                 var data = JSON.parse(jsonString);
-//                 if (data.Id === id) {
-//                     return data
-//                 }
-//             } catch (err) {
-//                 console.log('Error parsing JSON', err);
-//             }
-            
-//         }
-//     })
+// app.get('/transactions/:id', (req, res) => {
+//     const transactions = parsedData1.filter(({Id}) => Id === (req.params.id));
+//     if (!transactions) res.status(404).send('Could not find transaction')
+
 // });
 
-// app.get('/tshirt', (req, res) => {
-//     res.status(200).send({
-//         tshirt: '👕',
-//         size: 'large'
-//     })
-// })
+app.get('/transactions/:id', (req, res) => {
+    const transactions = parsedData2.filter(({Id}) => Id === (req.params.id));
+    if (!transactions) res.status(404).send('Could not find transaction')
 
-// app.post('/tshirt/:id', (req, res) => {
-//     const { id } = req.params;
-//     const { logo } = req.body;
-
-//     if (!logo) {
-//         res.status(418).send({ message: 'We need a logo!'})
-
-//     }
-
-//     res.send({
-//         tshirt: `👕 with your ${logo} and ID of ${id}`,
-//     });
-// });
+});

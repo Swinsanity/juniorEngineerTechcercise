28e922281e4eabe5936729287d2535919b12150a
Jesse Swinson <jesseswinsanity@gmail.com>
2022-05-26 14:01:06 -0400

    Figured out the problem and used filter to get specific data from the JSON array.

--- index.js
+++ index.js
@@ -4,30 +4,28 @@ const http = require('http');
 const app = express();
 const fs = require('fs');
 const PORT = process.env.PORT || 8080;
-const jsonData1 = fs.readFileSync('transactions.json', 'utf-8')
-const parsedData1 = JSON.parse(jsonData1)
-const jsonData2 = fs.readFileSync('oneTransaction.json', 'utf-8')
-const parsedData2 = JSON.parse(jsonData2)
+const jsonData = fs.readFileSync('transactions.json', 'utf-8')
+const parsedData = JSON.parse(jsonData)
+let asArray = Object.entries(parsedData)
+
 
 app.use( express.json() )
 
 app.listen(PORT, () => console.log(`It's alive! on http://localhost:${PORT}`)
 );
 
+//This route pulls all transactions
 app.get('/transactions', (req, res) => {
     
-    console.log(parsedData1);
-    res.send(parsedData1)
+    console.log(asArray);
+    res.send(asArray)
     });
 
-// app.get('/transactions/:id', (req, res) => {
-//     const transactions = parsedData1.filter(({Id}) => Id === (req.params.id));
-//     if (!transactions) res.status(404).send('Could not find transaction')
-
-// });
-
+//Route that pulls specific tranaction based on ID number
 app.get('/transactions/:id', (req, res) => {
-    const transactions = parsedData2.filter(({Id}) => Id === (req.params.id));
-    if (!transactions) res.status(404).send('Could not find transaction')
+    const results = parsedData.filter(({$id}) => $id === (req.params.id))
+    if (!results) res.status(404).send("Could not find transaction")
+    res.send(JSON.stringify(results))
+    console.log(JSON.stringify(results))
+})
 
-});
